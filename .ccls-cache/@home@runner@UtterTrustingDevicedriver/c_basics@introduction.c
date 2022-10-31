816ad957047b8706b1021973cdda5a6080958100
SHERMUKHAMMADTE <>
2022-10-31 06:47:30 +0000

    I wrote test program!

---
+++ .ccls-cache/@home@runner@UtterTrustingDevicedriver/c_basics@introduction.c
@@ -0,0 +1,7 @@
+#include <stdio.h>;
+
+main(){
+  printf("Salom dunyo");
+  return 0;
+  
+}
---
+++ .ccls-cache/@home@runner@UtterTrustingDevicedriver/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+  printf("SalomDunyo\nSalom C !");
+  printf("\nSinab ko'ramiz\nsinab ko'ramiz\nsinab ko'ramiz!");
+  return 0; 
+
+}
