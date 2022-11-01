2a20d5efc7ffb223dd5b04c9207f300683e05992
Student <student@iMac-Astrum-338.local>
2022-11-01 15:28:41 +0500

    I learned lesson4

---
+++ lesson4/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    // int a = 10;
+    // if(a % 10 != 0){
+    //     printf("It is wor!k");
+    // }
+
+    // else{
+    //     printf("else(){} is working!");
+    // }
+
+
+    int son = 5;
+
+    if(son > 10){
+        printf("It is working!");
+    }
+
+    else if(son < 10){
+        printf("else if is work!");
+    }
+
+    else{
+        printf("elsi is work!");
+    }
+    
+    return 0;
+}
